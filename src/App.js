ec4ab5b2713e7efb6040a147c763b69c125ac9a0
fwcorp <gseongkyu79@gmail.com>
2020-06-11 10:55:24 +0900

    saving current state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 import Log from "./components/Log/Log";
 import Stat from "./components/Stat/Stat";
 import LoginScreen from "./components/Login/LoginScreen";
-import SimpleBottomNavigation from "./components/SimpleBottomNavigation";
+import SimpleBottomNavigation from "./components/Nav/SimpleBottomNavigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Log1 from "./components/dummy/Log1";
-import Stat1 from "./components/dummy/Stat1";
+import ButtonAppBar from "./components/Nav/ButtonAppBar";
 
 const today = new Date().toDateString();
 
@@ -14,7 +13,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <LoginScreen />
+        {/* <LoginScreen /> */}
+        <ButtonAppBar />
         <h3>{today}</h3>
         <Switch>
           <Route path="/" exact component={Log} />
